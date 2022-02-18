a331f76f410ffe767b704d382b9c156e4fe2cabb
Paul <paul.quebeck@gmail.com>
2022-02-18 15:28:24 +0100

    TP1
    TP1

---
+++ Syracuse.py
@@ -0,0 +1,15 @@
+def syrac(n):
+ 
+    listsyrac=list()
+    if (type(n) != type(1) or n<1):
+        return None 
+ 
+    while n!=1:
+        if n%2 == 0:
+            n=n/2
+        else:
+          n=(n*3)+1
+        print(n)
+
+n=int(input())
+syrac(n)
